a86ac0eaa64ae6c65ad1e48e4a5054c1d4fd8db1
Kyle Sayers <kylesayrs@gmail.com>
2024-07-15 22:30:51 -0400

    initial commit

---
+++ src/main/java/ReadWriteLock/ReadWriteLock.java
@@ -0,0 +1,72 @@
+package ReadWriteLock;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+
+public class ReadWriteLock {
+    private final ReentrantLock lock = new ReentrantLock();
+    private final Condition readersOkay = lock.newCondition();
+    private final Condition writersOkay = lock.newCondition();
+    private int numReaders = 0;
+    private int numWriters = 0;
+
+    public void aquireReadLock() throws InterruptedException {
+        lock.lock();
+        try {
+            // cannot read while writers are writing
+            while (numWriters > 0) { 
+                // atomically suspends thread and releases lock
+                readersOkay.await();
+                // reaquires lock before returning to thread
+            }
+            numReaders += 1;
+            
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void releaseReadLock() {
+        lock.lock();
+        try {
+            numReaders -= 1;
+            // notify one writer to start
+            writersOkay.signal();
+
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void aquireWriteLock() throws InterruptedException {
+        lock.lock();
+        try {
+            // cannot write while readers are reading or writers are writing
+            while (numReaders >= 0 || numWriters >= 0) {
+                // atomically suspends thread and releases lock
+                writersOkay.await();
+                // requires lock before returning to thread
+            }
+            numWriters += 1;
+
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void releaseWriteLock() {
+        lock.lock();
+        try {
+            numWriters -= 1;
+
+            // give both readers and writers opportunities to run
+            // preventing starvation
+            readersOkay.signalAll();
+            writersOkay.signal();
+            
+        } finally {
+            lock.unlock();
+        }
+    }
+}
